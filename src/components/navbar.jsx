9cedc4c0c9d4d3d76092b9a9f845d3c7350850ea
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-12-05 19:35:46 +0100

    Add titles to navbar links for improved accessibility and wrap navbar in a <nav> element

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,21 +3,23 @@ import Logo from '../assets/logo/LogoW.svg';
 
 function Navbar() {
     return (
+        <nav>
         <div className={style.navbar}>
 
             <img className={style.logoimg} src={Logo} alt="Logo" />
 
             <div className={style.navItems}>
-            <a href="#Projects">Projects</a>
+            <a title='Projects' href="#Projects">Projects</a>
             <p>-</p>
-            <a href="#Skills">Skills</a>
+            <a title='Skills' href="#Skills">Skills</a>
             <p>-</p>
-            <a href="#AboutMe">Introduction</a>
+            <a title='About Me' href="#AboutMe">Introduction</a>
             <p>-</p>
-            <a href="#Contact">Contact</a>
+            <a title='Contact' href="#Contact">Contact</a>
             </div>
        
         </div>
+        </nav>
     );
     }
 
